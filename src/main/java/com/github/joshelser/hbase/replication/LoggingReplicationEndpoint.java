aa6dc881cfc091e3de84bcae4f314bd287a17fde
Josh Elser <jelser@cloudera.com>
2022-06-28 16:00:00 -0400

    HBase 2.4.x version

---
+++ src/main/java/com/github/joshelser/hbase/replication/LoggingReplicationEndpoint.java
@@ -0,0 +1,34 @@
+package com.github.joshelser.hbase.replication;
+
+import org.apache.hadoop.hbase.replication.HBaseReplicationEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingReplicationEndpoint extends HBaseReplicationEndpoint {
+  private static final Logger LOG = LoggerFactory.getLogger(LoggingReplicationEndpoint.class);
+
+
+  @Override
+  public boolean replicate(ReplicateContext replicateContext) {
+    LOG.info("replicate() called");
+    replicateContext.getEntries().stream().forEach((e) -> LOG.info("Replacing {} -> {}", e.getKey(), e.getEdit()));
+    return true;
+  }
+
+  @Override
+  public void start() {
+    LOG.info("Starting endpoint");
+    super.start();
+  }
+
+  @Override
+  public void stop() {
+    LOG.info("Stopping endpoint");
+    super.stop();
+  }
+
+  @Override
+  public boolean canReplicateToSameCluster() {
+    return true;
+  }
+}
